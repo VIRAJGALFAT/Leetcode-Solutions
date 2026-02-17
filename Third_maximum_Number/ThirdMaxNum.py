e3fd6e9aae35c5f0fc599815a4dac1606a8ba791
VIRAJGALFAT <virjgalfat846@gmail.com>
2026-02-17 23:03:10 +0530

    pdf is added

--- Third_maximum_Number/ThirdMaxNum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def thirdMax(self, nums: List[int]) -> int:
-        s = set(nums)
-        lst = sorted(s, reverse=True)
-        
-        if len(lst) >= 3:
-            return lst[2]
-        else:
-            return lst[0]
